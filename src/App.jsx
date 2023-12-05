5c950aa6f7b49ba624a07513293df38796c6505f
balavignesh111 <balavignesh1018@gmail.com>
2023-12-05 17:38:11 +0530

    login and sign up ui completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,130 +2,132 @@ import QuizBox from './component/quizBox'
 import QuestionSideBar from './component/questionSideBox'
 import Congratulation from './component/congrats.component';
 import { useEffect, useState } from 'react'
+import Login from './component/login.component';
+import Register from './component/register.component';
 // import data from './constants';
 
 function App() {
   // functions
   // to generate random number
-  const getRandomNo = ()=>{
-    return Math.trunc(Math.random()*4);
-  }
-  // shuffled answer list
-  const answerList = (index)=>{
-    let list = [...db[index]?.incorrect_answers]
-    list.splice(getRandomNo(),0,db[index]?.correct_answer);
-    return list;
-  }
+  // const getRandomNo = ()=>{
+  //   return Math.trunc(Math.random()*4);
+  // }
+  // // shuffled answer list
+  // const answerList = (index)=>{
+  //   let list = [...db[index]?.incorrect_answers]
+  //   list.splice(getRandomNo(),0,db[index]?.correct_answer);
+  //   return list;
+  // }
 
-  // modifies the data structure
-  const modifiedData = (data)=>{
-    return data?.map((ele,ind)=>{
-      const obj = {
-        ...ele
-      }
-      obj["selectedOption"] = undefined;
-      obj["answerList"] = answerList(ind);
-      return obj;
-    })
-  }
+  // // modifies the data structure
+  // const modifiedData = (data)=>{
+  //   return data?.map((ele,ind)=>{
+  //     const obj = {
+  //       ...ele
+  //     }
+  //     obj["selectedOption"] = undefined;
+  //     obj["answerList"] = answerList(ind);
+  //     return obj;
+  //   })
+  // }
   
-  const url = "https://opentdb.com/api.php?amount=10";
-  // useState hooks for updating questions and answers
-  const [db, setDb] = useState([]);
+  // const url = "https://opentdb.com/api.php?amount=10";
+  // // useState hooks for updating questions and answers
+  // const [db, setDb] = useState([]);
+  // // console.log(db)
+  // const [index, setIndex] = useState(0);
+  // const [qtn, setQtn] = useState(0);
+  // const [ansList,setAnsList] = useState([]);
+  // const [userChoice,setUserChoice]= useState('');
+  // const [score,setScore] = useState(0);
+  // const [isSubmitted,SetIsSubmitted] = useState(false);
+  // const [highScore,setHighScore] = useState(0);
+
+  // // use effect for fetching api
+  // useEffect(()=>{
+  //   const fetchData = async ()=>{
+  //     const resp = await fetch(url);
+  //     const {results} = await resp.json();
+  //     setDb(modifiedData(results))
+  //   }
+  //   fetchData();  
+  // },[])
   // console.log(db)
-  const [index, setIndex] = useState(0);
-  const [qtn, setQtn] = useState(0);
-  const [ansList,setAnsList] = useState([]);
-  const [userChoice,setUserChoice]= useState('');
-  const [score,setScore] = useState(0);
-  const [isSubmitted,SetIsSubmitted] = useState(false);
-  const [highScore,setHighScore] = useState(0);
 
-  // use effect for fetching api
-  useEffect(()=>{
-    const fetchData = async ()=>{
-      const resp = await fetch(url);
-      const {results} = await resp.json();
-      setDb(modifiedData(results))
-    }
-    fetchData();  
-  },[])
-  console.log(db)
+  // useEffect(()=>{
+  //   setQtn(db[index]?.question);
+  //   setAnsList(db[index]?.answerList);
+  //   console.log(db)
+  // },[db])
 
-  useEffect(()=>{
-    setQtn(db[index]?.question);
-    setAnsList(db[index]?.answerList);
-    console.log(db)
-  },[db])
+  // // next btn
+  // const nextBtn = ()=>{
+  //   if(index === db.length - 1){
+  //     setIndex(()=> 0);
+  //   }else if(index > 0 || index < db.length-1){
+  //     setUserChoice(()=>(''));
+  //     setIndex((index)=> (index += 1))
+  //   }
+  // }
 
-  // next btn
-  const nextBtn = ()=>{
-    if(index === db.length - 1){
-      setIndex(()=> 0);
-    }else if(index > 0 || index < db.length-1){
-      setUserChoice(()=>(''));
-      setIndex((index)=> (index += 1))
-    }
-  }
-
-  // prev btn
-  const prevBtn = ()=>{
-    if(index <= 0){
-      setIndex(()=>(db.length-1))
-    }else if(index > 0 || index <= db.length-1){
-      // updates previously selected val
-      setIndex((index)=> (index -= 1))
-    }
-  }
+  // // prev btn
+  // const prevBtn = ()=>{
+  //   if(index <= 0){
+  //     setIndex(()=>(db.length-1))
+  //   }else if(index > 0 || index <= db.length-1){
+  //     // updates previously selected val
+  //     setIndex((index)=> (index -= 1))
+  //   }
+  // }
 
 
 
-  useEffect(()=>{
-    setQtn(()=>(db[index]?.question))
-    setAnsList(db[index]?.answerList);
-  },[index])
+  // useEffect(()=>{
+  //   setQtn(()=>(db[index]?.question))
+  //   setAnsList(db[index]?.answerList);
+  // },[index])
 
-  // func to update db
-  const updateDb = (val,index,db)=>{
-    setDb(()=>{
-      return (db.map((ele,ind)=>{
-        if(index === ind){
-          ele.selectedOption = val;
-          console.log(ele);
-          return ele;
-        }
-        return ele;
-      }))
-    })
-    console.log(db)
-  }
+  // // func to update db
+  // const updateDb = (val,index,db)=>{
+  //   setDb(()=>{
+  //     return (db.map((ele,ind)=>{
+  //       if(index === ind){
+  //         ele.selectedOption = val;
+  //         console.log(ele);
+  //         return ele;
+  //       }
+  //       return ele;
+  //     }))
+  //   })
+  //   console.log(db)
+  // }
 
-  // retake quiz function
-  const init = ()=>{
-    setHighScore(score);
-    setScore(0);
-    SetIsSubmitted(()=>(false));
-    setIndex(0);
-    setDb(modifiedData());
-  }
+  // // retake quiz function
+  // const init = ()=>{
+  //   setHighScore(score);
+  //   setScore(0);
+  //   SetIsSubmitted(()=>(false));
+  //   setIndex(0);
+  //   setDb(modifiedData());
+  // }
 
-  // score
-  const submitQuiz = () =>{
-    let res = db.reduce((acc,ele)=>{
-      if(ele.selectedOption === ele.correct_answer){
-        acc += 1;
-      }
-      return acc;
-    },0)
-    setScore(()=>res);
-    return res;
-  }
+  // // score
+  // const submitQuiz = () =>{
+  //   let res = db.reduce((acc,ele)=>{
+  //     if(ele.selectedOption === ele.correct_answer){
+  //       acc += 1;
+  //     }
+  //     return acc;
+  //   },0)
+  //   setScore(()=>res);
+  //   return res;
+  // }
   
 
 
   return (
-    <div className='w-[100%] min-h-full p-4'>
-      <h1 className='text-center font-semibold text-[2rem]'>Quiz Title</h1>
+    <div className='w-[100%] min-h-full'>
+      {/* <h1 className='text-center font-semibold text-[2rem]'>Quiz Title</h1>
       <div className='w-[100%] flex flex-row justify-around'>
         <QuizBox 
           answerList = {ansList}
@@ -145,7 +147,9 @@ function App() {
             retryHandler = {init} 
             score = {score}
             highScore = {highScore}
-          /> : ''}
+          /> : ''} */}
+      {/* <Login/> */}
+      <Register/>
     </div>
   )
 }
--- src/component/register.component.jsx
+++ src/component/register.component.jsx
@@ -1,11 +1,22 @@
 import { MdOutlineMailLock } from "react-icons/md";
 import { RiLockPasswordLine  } from "react-icons/ri";
-import { FcGoogle } from "react-icons/fc";
-const Login = () => {
-  return ( 
-  <div className="w-full min-h-[100vh] flex flex-col justify-center items-center bg-blue-300">
+import { CgProfile } from "react-icons/cg";
+const Register = () => {
+  return (
+    <div className="w-full min-h-[100vh] flex flex-col justify-center items-center bg-blue-300">
     <div className="w-[25rem] border-2 border-black p-4 flex flex-col justify-evenly items-center rounded-lg shadow-xl bg-gray-100">
-      <h2 className="text-center text-[1.25rem] font-bold">SIGN IN</h2>
+      <h2 className="text-center text-[1.25rem] font-bold">SIGN UP</h2>
+      <div className="w-full flex flex-col justify-center items-start px-[1rem] py-2 gap-1">
+        <label htmlFor="email">Username</label>
+        <div className="w-full relative">
+          <div className="absolute top-3 left-2">
+            <CgProfile className="text-[1.25rem]"/>
+          </div>
+          <div className="w-full">
+            <input type="text" id="username" className="pl-[2.25rem] p-[0.5rem] w-full rounded-md border-2 border-black" placeholder="Enter your name"></input>
+          </div>
+        </div>
+      </div>
       <div className="w-full flex flex-col justify-center items-start px-[1rem] py-2 gap-1">
         <label htmlFor="email">Email</label>
         <div className="w-full relative">
@@ -23,30 +34,16 @@ const Login = () => {
           <div className="absolute top-3 left-2">
             <RiLockPasswordLine className="text-[1.25rem]"/>
           </div>
-          <input type="password" className="pl-[2.25rem] p-[0.5rem] w-full rounded-md border-2 border-black" id="password" placeholder="Enter Password"></input>
+          <input type="password" className="pl-[2.25rem] p-[0.5rem] w-full rounded-md border-2 border-black" id="password" placeholder="Create Password"></input>
         </div>
-        
       </div>
       <div className="px-4 py-2 w-full mt-2">
         <button className="w-full px-4 py-1 bg-blue-300 rounded-md border-2 border-black">
-          Login
+          Create Account
         </button>
-      </div>
-      <div className="w-full px-4 py-2 mt-8">
-        <div className="w-full border-t-2 border-black flex flex-row justify-center items-center relative">
-          <div className="w-[2rem] text-center border-2 border-black bg-white absolute top-[-1rem]">OR</div>
-        </div>
-      </div>
-      <div className="w-full px-4 py-2 flex justify-center items-center">
-        <button className="w-[3rem] h-[3rem] border-2 rounded-[50%] flex justify-center items-center"><FcGoogle className="text-[3rem]"/></button>
-      </div>
-      <div className="">
-        <p className="">
-          New User? <a href="#">Register</a>
-        </p>
       </div>
     </div>
   </div> );
 }
-
-export default Login;
+ 
+export default Register;
